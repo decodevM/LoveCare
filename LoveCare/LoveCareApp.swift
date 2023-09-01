38daa170d2a39d97462b385a4397a74f9094c2ce
devodevM <decodev.contact@gmail.com>
2023-09-01 15:03:14 +0100

    Initial Commit

---
+++ LoveCare/LoveCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveCareApp.swift
+//  LoveCare
+//
+//  Created by Yacine on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LoveCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
